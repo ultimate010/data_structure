090148215cf666ac81beae347ba419d535a94639
ultimate010 <ultimate010@gmail.com>
2013-08-19 09:56:49 +0800

    添加插入排序

---
+++ sort/insertSort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void insertSort(int * arr,int len){
+    int pos;
+    for(pos = 1;pos < len;pos++){
+      if(arr[pos] < arr[pos - 1]){
+        int temp = arr[pos]; int posJ;
+        for(posJ = pos;posJ > 1 && arr[posJ - 1] > temp;posJ--){
+          arr[posJ] = arr[posJ - 1];
+        }
+        arr[posJ] = temp;
+      }
+    }
+}
+
+int main(){
+  int a[11] = {10,21,1232,12321,123,21,2132,213213,213,21321321,321};
+  int i = 0;
+  for(i = 0;i < 11;i++){
+    printf("%d\t",a[i]);
+  }
+  insertSort(a,11);
+  printf("\n",a[i]);
+  for(i = 0;i < 11;i++){
+    printf("%d\t",a[i]);
+  }
+  return 0;
+}
