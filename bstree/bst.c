d99da06dd78c629708628bf94cbb772b5d86ba8a
ultimate010 <ultimate010@gmail.com>
2014-01-04 17:37:01 +0800

    Add binary sort tree

---
+++ bstree/bst.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include "bst.h"
+
+status compare_for_int(void * a,void * b){
+  int * pa = (int *) a;
+  int * pb = (int *) b;
+  if(*pa > *pb){
+	return GREATER;
+  }else if(*pa == *pb){
+	return EQUAL;
+  }else{
+	return LOWER;
+  }
+}
+void myPrint(void * a){
+  int * pa = (int *) a;
+  printf("%d\t",*pa);
+}
+
+int main(){
+  bstree tree;
+  ini_bstree(&tree); //初始化
+  int i;
+  int * pData;
+  for(i = 0;i < 36;i++){
+	pData = (int *) malloc(sizeof(int));
+	*pData = i;
+	if(insert_bstree(&tree,(void *)pData,compare_for_int) != TRUE){
+	  free(pData);
+	}
+  }
+  printf("Insert [0,36) tree is \n");
+  inOrder_bstree(tree,myPrint);
+  for(i = 40;i > 1;i--){
+	pData = (int *) malloc(sizeof(int));
+	*pData = i;
+	if(insert_bstree(&tree,(void *)pData,compare_for_int) != TRUE){
+	  free(pData);
+	}
+  }
+  printf("\n");
+  printf("Insert (1,40] tree is \n");
+  inOrder_bstree(tree,myPrint);
+  printf("\n");
+  pData = (int *) malloc(sizeof(int));
+  for(i = 23;i < 30;i++){
+	*pData = i;
+	if(delete_bstree(&tree,(void *)pData,compare_for_int) != TRUE){
+	  printf("ERROR");
+	}
+  }
+  printf("After delete [23,30)\n");
+  inOrder_bstree(tree,myPrint);
+  printf("\n");
+  for(i = 53;i >= 0;i--){
+	*pData = i;
+	if(delete_bstree(&tree,(void *)pData,compare_for_int) != TRUE){
+	  printf("NO %d\t",*pData);
+	}
+  }
+  printf("\n");
+  printf("After delete [53,0]\n");
+  inOrder_bstree(tree,myPrint);
+  printf("\n");
+  free(pData);
+  if(empty_bstree(&tree) == TRUE){
+	printf("The tree is empty\n");
+  }
+  free_bstree(&tree);
+
+  return 0;
+}
+
---
+++ bstree/bst.h
@@ -0,0 +1,169 @@
+/*
+ * 二叉排序树
+ * Zaks Wang
+ * 2014-1-4
+ */
+#ifndef __BSTREE_H_
+#define __BSTREE_H_
+#include <stdlib.h>
+#include "def.h"
+typedef struct bsnode{
+  void * m_data;
+  struct bsnode * m_left;
+  struct bsnode * m_right;
+}bsnode, * bstree;
+/*	public func define	*/
+typedef status (*compareFunc)(void * a,void * b); //定义比较函数
+typedef void (*visitFunc)(void * a); //遍历访问函数
+status ini_bstree(bstree * pRoot); //初始化操作,赋值为NULL
+status insert_bstree(bstree * pRoot,void * value,compareFunc func); //插入元素操作
+status delete_bstree(bstree * pRoot,void * value,compareFunc func); //删除元素操作
+status find_bstree(bstree * pRoot,void * value,compareFunc func); //查找元素是否存在
+status inOrder_bstree(bstree pRoot,visitFunc visit); //中序遍历函数
+status free_bstree(bstree * pRoot); //清空树
+status empty_bstree(bstree * pRoot); //判断是否为空
+/*	private func define	*/
+void _free_bstree(bstree p);
+void _inOrder_bstree(bstree p,visitFunc visit);
+/*	implement	*/
+status empty_bstree(bstree * pRoot){
+  return (*pRoot == NULL)?TRUE:FALSE;
+}
+/*	private func define	*/
+/*
+ * 初始化操作
+ * @pRoot 根指针
+ * @return 总是返回TRUE
+ */
+status ini_bstree(bstree * pRoot){
+  *pRoot = NULL;
+  return TRUE;
+}
+/*
+ * 插入函数
+ * @pRoot 树的根指针
+ * @value 待插入的数据
+ * @func 比较函数,a == b 返回 EQUAL, a > b 返回 GREATER, a < b 返回 LOWER
+ * @return	当存在相同节点时候返回false,内存不够返回OVERFLOW
+ */
+status insert_bstree(bstree * pRoot,void * value,compareFunc compare){
+  bstree * p = pRoot;
+  while(*p){
+	if(compare((*p)->m_data,value) == EQUAL){ //当前等于value
+	  //存在相同节点,返回错误
+	  return FALSE;
+	}else if(compare((*p)->m_data,value) == GREATER){
+	  p = &((*p)->m_left); //指向左子树的那个数据的指针
+	}else{
+	  p = &((*p)->m_right);
+	}
+  }
+  *p = (bsnode *) malloc(sizeof(bsnode));
+  if(*pRoot == NULL){
+	return OVERFLOW;
+  }
+  (*p)->m_data = value;
+  return TRUE;
+}
+/*
+ * 删除函数
+ * @pRoot 树的根指针
+ * @value 待删除的值
+ * @func 比较函数
+ * @return 找到并删除返回TRUE,找不到返回FALSE
+ */
+status delete_bstree(bstree * pRoot,void * value,compareFunc compare){
+  bstree * p = pRoot;
+  while(*p){
+	if(compare((*p)->m_data,value) == EQUAL){
+	  //找到待删除的元素
+	  bstree * pLeft = &((*p)->m_left); //指向左孩子的指针的指针
+	  while(*pLeft){
+		pLeft = &((*pLeft)->m_right);//找到左孩子的最右
+	  }
+	  *pLeft = (*p)->m_right; //把待删除节点的右孩子接过去
+	  bstree pDel = (*p); //指向待删除节点的指针
+	  *p = pDel->m_left;
+	  free(pDel); //释放待删除节点
+	  return TRUE;
+	}else if(compare((*p)->m_data,value) == GREATER){
+	  p = &((*p)->m_left);
+	}else{
+	  p = &((*p)->m_right);
+	}
+  }
+  return FALSE;
+}
+/*
+ * 查找函数
+ * @pRoot 树的根指针
+ * @value 待查找的元素
+ * @func 比较函数
+ * @return 找到返回TRUE,找不到返回FALSE
+ */
+status find_bstree(bstree * pRoot,void * value,compareFunc compare){
+  bstree * p = pRoot;
+  while(*p){
+	if(compare((*p)->m_data,value) == EQUAL){
+	  return TRUE;
+	}else if(compare((*p)->m_data,value) == GREATER){
+	  p = &((*p)->m_left);
+	}else{
+	  p = &((*p)->m_right);
+	}
+  }
+  return FALSE;
+}
+/*
+ * 清空树操作
+ * @pRoot 树的根指针
+ * @return 成功返回TRUE,失败返回FALSE
+ */
+status free_bstree(bstree * pRoot){
+  if(*pRoot){
+	_free_bstree(*pRoot);
+	*pRoot = NULL;
+	return TRUE;
+  }else{
+	return FALSE; //空树
+  }
+}
+/*
+ * 清空辅助函数
+ * @p 待清空的树指针
+ */
+void _free_bstree(bstree p){
+  if(p){
+	_free_bstree(p->m_left);
+	_free_bstree(p->m_right);
+	free(p);
+  }
+}
+/*
+ * 中序遍历函数
+ * @pRoot 根指针
+ * @visit 访问函数
+ * @return 成功返回TRUE,错误返回FALSE
+ */
+status inOrder_bstree(bstree pRoot,visitFunc visit){
+  if(pRoot){
+	_inOrder_bstree(pRoot,visit);
+	return TRUE;
+  }else{
+	return FALSE;
+  }
+}
+/*
+ * 中序遍历函数
+ * @p 指针
+ * @visit 访问函数
+ */
+void _inOrder_bstree(bstree p,visitFunc visit){
+  if(p){
+	_inOrder_bstree(p->m_left,visit);
+	visit(p->m_data);
+	_inOrder_bstree(p->m_right,visit);
+  }
+}
+
+#endif
